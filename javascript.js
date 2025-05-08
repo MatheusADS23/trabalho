9cb598af358464c5e03350ab73420f9682545fdc
MatheusADS23 <matheusrobertomuller@gmail.com>
2025-05-08 18:29:00 -0300

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,6 @@
+const botao = document.getElementById('botaomsg');
+const mensagem = document.getElementById('msg');
+
+botao.addEventListener('click', function() {
+    mensagem.textContent = "Você é capaz de tudo, acredite em si mesmo!";
+});
